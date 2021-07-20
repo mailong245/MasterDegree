90f82cf9e3046c7ff7e6ce34493d09cecc8d9b75
Long.MaiThoai <Long.MaiThoai@nttdata.com>
2021-07-20 18:08:14 +0700

    update

--- src/AlgorithmApplication.java
+++ src/AlgorithmApplication.java
@@ -1,23 +1,32 @@
 import sorting.impl.HeapSort;
-import sorting.impl.InsertionSort;
-import sorting.impl.QuickSort;
-import sorting.impl.SelectionSort;
 import util.Utils;
 
 public class AlgorithmApplication {
 
-    public static void main(String[] args) {
-        System.out.print("Array: ");
-        Utils.print(Utils.arr);
+  public static void main(String[] args) {
+    System.out.print("Array: ");
+    Utils.print(Utils.arr);
 
 //        BubbleSort bubbleSort = new BubbleSort();
 //        QuickSort sortAlgorithm = new QuickSort();
 //        InsertionSort sortAlgorithm = new InsertionSort();
 //        SelectionSort sortAlgorithm = new SelectionSort();
 
-        HeapSort sortAlgorithm = new HeapSort();
-        sortAlgorithm.sort(Utils.arr);
+    HeapSort sortAlgorithm = new HeapSort();
+    sortAlgorithm.sort(Utils.arr);
 
-        Utils.print(Utils.arr);
+    Utils.print(Utils.arr);
+
+    int result = fibo(7);
+    System.out.println("Fibo: " + result);
+  }
+
+  private static int fibo(int n) {
+    if (n <= 2) {
+      return 1;
+    } else {
+      return fibo(n - 1) + fibo(n - 2);
     }
+  }
+
 }
